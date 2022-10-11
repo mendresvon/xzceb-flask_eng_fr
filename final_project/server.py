642863baa2f612d0f7b3497229663be9dfc10abe
Mendrezzzzz <mendresvon@gmail.com>
2022-10-11 16:38:59 +0800

    Before changing everything

--- final_project/server.py
+++ final_project/server.py
@@ -3,26 +3,30 @@ from flask import Flask, render_template, request
 import json
 import machinetranslation
 
-app = Flask("Web Translator")
+app = Flask("Web Translator", template_folder="templates")
+
 
 @app.route("/englishToFrench")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
+    textToTranslate = request.args.get("textToTranslate")
     # Write your code here
     french_translation = englishToFrench(textToTranslate)
     return f"Translated text to French SUCCESS\nEnglish: {textToTranslate}\nFrench: {french_translation}"
 
+
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
+    textToTranslate = request.args.get("textToTranslate")
     # Write your code here
     english_translation = frenchToEnglish(textToTranslate)
     return f"Translated text to English SUCCESS\nFrench: {textToTranslate}\nEnglish: {english_translation}"
 
+
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
